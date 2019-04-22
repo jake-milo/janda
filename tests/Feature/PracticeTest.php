366de0230db999ab15289427f1fb52d7c83ec7d7
Milo Halsey-Perry <milo.halseyperry@gmail.com>
2019-04-22 20:26:05 +0100

    Added restore test for practice.

--- tests/Feature/PracticeTest.php
+++ tests/Feature/PracticeTest.php
@@ -37,4 +37,45 @@ class PracticeTest extends TestCase
             ],
         ]);
     }
+
+    public function testUserCanUpdatePractice()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $practice = factory(Practice::class)->create();
+        $updates = factory(Practice::class)->make();
+
+        $this->patch("/api/practices/{$practice->id}", $updates->attributesToArray());
+
+        $this->assertDatabaseHas('practices', $updates->attributesToArray());
+    }
+
+    public function testUserCanDeletePractice()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $practice = factory(Practice::class)->create();
+
+        $response = $this->delete("api/practices/{$practice->id}");
+
+        $response->assertStatus(200);
+
+        $this->assertSoftDeleted($practice);
+    }
+
+    public function testUserCanRestorePractice()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $practice = factory(Practice::class)->create();
+
+        $practice->delete();
+
+        $this->post("/api/practices/{$practice->id}/restore");
+
+        $this->assertFalse($practice->fresh()->trashed());
+    }
 }
