3b32a82a96acfc3950ad012282f2454e2659e72e
Jake Taylor <me@jaketaylor.co>
2019-04-27 01:27:26 +0100

    Start building create lab order form

--- resources/js/pages/LabOrders/index.js
+++ resources/js/pages/LabOrders/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import RoundAdd from 'react-md-icon/dist/RoundAdd';
 import { PageTitle } from '../../components/PageTitle';
 import { Page } from '../../components/Page';
 import { Table, Row, Cell } from '../../components/Table';
 import { Spinner } from '../../components/Spinner';
 import { Pagination } from '../../components/Pagination';
+import { FloatingActionButton } from '../../components/FloatingActionButton';
 import { useLabOrders } from './useLabOrders';
 import { usePracticeFilter } from '../../hooks/usePracticeFilter';
 import { useStatusFilter } from './useStatusFilter';
+import { CreateLabOrderModal } from './CreateLabOrderModal';
 
 export const LabOrders = () => {
     const { status, statuses, handleStatusChange } = useStatusFilter();
     const { practice, practices, handlePracticeChange } = usePracticeFilter();
     const { labOrders, loading, page, pageCount } = useLabOrders({ practice, status });
+
+    const [showCreateModal, setShowCreateModal] = useState(false);
 
     return (
         <>
@@ -76,7 +81,7 @@ export const LabOrders = () => {
                                             {labOrder.lab.name}
                                         </Link>
                                     </Cell>
-                                    <Cell>{labOrder.reference}</Cell>
+                                    Lab Orders         <Cell>{labOrder.reference}</Cell>
                                 </Row>
                             ))}
                         </Table>
@@ -90,6 +95,12 @@ export const LabOrders = () => {
                 ) : (
                     <Spinner />
                 )}
+
+                <FloatingActionButton onClick={() => setShowCreateModal(true)}>
+                    <RoundAdd />
+                </FloatingActionButton>
+
+                <CreateLabOrderModal show={showCreateModal} hide={() => setShowCreateModal(false)} />
             </Page>
         </>
     );
