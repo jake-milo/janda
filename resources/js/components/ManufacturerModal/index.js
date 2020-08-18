28b1ff07a5b217e8b5161e1c48aba24c6aee3ded
Milo Halsey-Perry <milo.halseyperry@gmail.com>
2020-08-18 22:07:19 +0100

    converted Manufacturer form.

--- resources/js/components/ManufacturerModal/index.js
+++ resources/js/components/ManufacturerModal/index.js
@@ -1,70 +1,82 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import * as yup from 'yup';
 import { useInitialValues } from './useInitialValues';
 import { Spinner } from '../Spinner';
-import { Form } from '../Form';
-import { FieldError } from '../FieldError';
+import { FormNew } from '../Form';
+import { FieldErrorNew } from '../FieldError';
 import { Modal } from '../Modal';
 import { PageTitle } from '../PageTitle';
 import { post, patch } from '../../helpers';
+import { fetchManufacturer } from '../../utilities/fetchManufacturer';
+import { useForm } from '../../hooks/useForm';
 
 const schema = yup.object().shape({
     name: yup.string().required().label('Name'),
 });
 
 export const ManufacturerModal = ({ show, hide, onSuccess, editing }) => {
-    const isProvided = editing && typeof editing === 'object';
-    const [initialValues, loading, resetInitialValues] = useInitialValues(editing, isProvided);
+    console.log(editing);
+    const getInitialValues = useCallback(async (id) => {
+        if (!id) return {
+            name: '',
+        };
 
-    const handleSubmit = (manufacturer, { setSubmitting }) => {
-        const id = () => typeof editing === 'object' ? editing.id : editing;
+        const manufacturer = await fetchManufacturer(id);
 
+        return {
+            name: manufacturer.name,
+        };
+    }, []);
+
+    const {
+        values,
+        loading,
+        createNativeHandler,
+        errors,
+        submitHandler,
+        isValid,
+    } = useForm({ editing, getInitialValues, schema });
+
+
+    const handleSubmit = submitHandler(() => {
         const request = () => editing
-            ? patch(`/api/manufacturers/${id()}`, manufacturer)
-            : post('/api/manufacturers', manufacturer);
+            ? patch(`/api/manufacturers/${editing}`, values)
+            : post('/api/manufacturers', values);
 
         request()
             .then(() => {
                 hide();
                 onSuccess();
-                setSubmitting(false);
             })
             .catch((err) => {
                 console.log(err);
-                setSubmitting(false);
             });
-    };
-
-    const onHide = () => {
-        hide();
-        resetInitialValues();
-    };
+    });
 
     return (
-        <Modal show={show} hide={onHide}>
-            <PageTitle>Create Manufacturer</PageTitle>
+        <Modal show={show} hide={hide}>
+            <PageTitle>{editing ? 'Update Manufacturer' : 'Create Manufacturer'}</PageTitle>
 
-            {loading && (
-                <Spinner />
-            )}
-
-            <Form
-                validationSchema={schema}
-                initialValues={initialValues}
-                enableReinitialize={!!editing}
-                onSubmit={handleSubmit}
-                render={({ handleSubmit: onSubmit, handleChange, values }) => (
-                    <form onSubmit={onSubmit} style={{ display: loading ? 'none' : 'block' }}>
+            <FormNew values={values} loading={loading} onSubmit={handleSubmit}>
+                {() => (
+                    <>
                         <div className="input-wrapper">
                             <label htmlFor="name">Name</label>
-                            <input type="text" id="name" name="name" onChange={handleChange} value={values.name} />
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                onChange={createNativeHandler('name')}
+                                value={values.name}
+                            />
                         </div>
-                        <FieldError name="name" />
+                        <FieldErrorNew name="name" errors={errors} />
 
-                        <input type="submit" value={editing ? 'Save' : 'Create'} />
-                    </form>
+                        <input type="submit" value={editing ? 'Save' : 'Create'} disabled={!isValid} />
+                    </>
                 )}
-            />
+
+            </FormNew>
         </Modal>
     );
 };
