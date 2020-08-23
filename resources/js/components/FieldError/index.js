83d9f68b6b70f8235e9dae7569fc8665999a0d01
Jake Taylor <me@jaketaylor.co>
2020-08-23 16:50:53 +0100

    Remove unused components/old code

--- resources/js/components/FieldError/index.js
+++ resources/js/components/FieldError/index.js
@@ -1,19 +1,5 @@
 import React, { useMemo } from 'react';
-import { connect, getIn } from 'formik';
 import { useFormContext } from '../Form';
-
-export const FieldError = connect(({ formik, name }) => {
-    const error = getIn(formik.errors, name);
-    const touched = getIn(formik.touched, name);
-
-    if (getIn(formik.values, name) === undefined) {
-        throw new Error(`Field ${name} not recognized (in FieldError component)!`);
-    }
-
-    return error && typeof error === 'string' && touched ? (
-        <p className="field-error">{error}</p>
-    ) : null;
-});
 
 export const FieldErrorNew = ({ name }) => {
     const { errors } = useFormContext();
--- resources/js/components/Form/index.js
+++ resources/js/components/Form/index.js
@@ -1,32 +1,5 @@
 import React, { createContext, useContext, useMemo } from 'react';
-import { Formik, yupToFormErrors, validateYupSchema } from 'formik';
 import { Spinner } from '../Spinner';
-
-export const Form = ({ validationSchema, ...props }) => (
-    <Formik
-        {...props}
-        validate={(vals, props) => {
-            console.log({ props, context: props.getContext(), vals });
-
-            return validationSchema
-                .validate(vals, { abortEarly: false, context: props.getContext() })
-                .then(vals => (console.log({vals}), vals))
-                .catch(err => {
-                    throw yupToFormErrors(err);
-                });
-
-            return validateYupSchema(vals, validationSchema, false, getContext(vals))
-                .then((newVals) => {
-                    console.log({ newVals });
-                    return newVals;
-                })
-                .catch((err) => {
-                    console.log({ err });
-                    throw yupToFormErrors(err);
-                })
-        }}
-    />
-);
 
 const FormContext = createContext();
 
