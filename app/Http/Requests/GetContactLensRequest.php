7e7be304d16f9c4f4631c750fdd6ba8454cb7721
Jake Taylor <me@jaketaylor.co>
2021-01-31 14:24:57 +0000

    Add brand filter to contact lenses, resolves #76

--- app/Http/Requests/GetContactLensRequest.php
+++ app/Http/Requests/GetContactLensRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Models\ContactLens;
 use App\Http\Requests\Concerns\HasOrdering;
+use App\Models\ContactLens\Brand;
 
 class GetContactLensRequest extends FormRequest
 {
@@ -13,8 +14,9 @@ class GetContactLensRequest extends FormRequest
     protected $relationSorts = [
         'patient' => ['patients', 'patient_id', 'name'],
         'practice' => ['practices', 'practice_id', 'name'],
+        'brand' => ['contact_lens_brands', 'brand_id', 'name'],
         'lab' => ['labs', 'lab_id', 'name'],
-        'type' => ['contact_lens_types','type_id', 'name'],
+        'type' => ['contact_lens_types', 'type_id', 'name'],
         'duration' => ['contact_lens_types', 'type_id', 'duration'],
     ];
 
@@ -27,6 +29,7 @@ class GetContactLensRequest extends FormRequest
     {
         return [
             'practice' => 'nullable|integer|exists:practices,id',
+            'brand' => 'nullable|integer|exists:contact_lens_brands,id',
             'sort' => 'nullable|string|in:patient,practice,type,duration,quantity,price,solutions',
             'order' => 'nullable|string|in:asc,desc',
         ];
@@ -39,6 +42,13 @@ class GetContactLensRequest extends FormRequest
 
         if ($practice = $this->input('practice')) {
             $query->where('practice_id', $practice);
+        }
+
+        if ($brand = $this->input('brand')) {
+            $brand = Brand::find($brand);
+            $typeIds = $brand->types()->get('id');
+
+            $query->whereIn('type_id', $typeIds);
         }
 
         return $query->paginate(30);
