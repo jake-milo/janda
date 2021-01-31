d8b3ad6c3252eae8906b7c39e196e13e95290a43
Jake Taylor <me@jaketaylor.co>
2021-01-31 14:41:05 +0000

    Make table modification use proper migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password')->nullable();
+            $table->string('password');
             $table->string('activation_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
