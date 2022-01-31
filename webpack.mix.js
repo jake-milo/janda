eed45b03109d4fb78716c0b28dd1c051d660d6ab
Jake Taylor <me@jaketaylor.co>
2022-01-31 16:08:21 +0000

    Compile prod assets with versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,13 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix
-    .react('resources/js/index.js', 'public/js/app.js')
-    .copyDirectory('resources/img', 'public/img')
-    .sass('resources/sass/login.scss', 'public/css/login.css')
+mix.react("resources/js/index.js", "public/js/app.js")
+    .copyDirectory("resources/img", "public/img")
+    .sass("resources/sass/login.scss", "public/css/login.css")
     .sourceMaps()
+    .version()
     .options({
         hmrOptions: {
-            host: 'janda.test',
-            port: '8000',
-        },
-    })
+            host: "janda.test",
+            port: "8000"
+        }
+    });
